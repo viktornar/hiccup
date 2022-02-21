63db156a53c33a801683a174bc262ba6869b2057
Viktor Nareiko <vnareiko.lt@gmail.com>
2022-02-21 23:49:36 +0200

    Upgraded checkstyle plugin and fixed configuration for it.

--- src/main/java/com/github/viktornar/hiccup/HiccupRunner.java
+++ src/main/java/com/github/viktornar/hiccup/HiccupRunner.java
@@ -4,11 +4,13 @@ import com.github.viktornar.hiccup.game.client.DragonOfMugloarClientV2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Profile("!test")
 public class HiccupRunner implements CommandLineRunner {
     private final DragonOfMugloarClientV2 dragonOfMugloarClientV2;
 
--- src/test/java/com/github/viktornar/hiccup/HiccupPropertiesTest.java
+++ src/test/java/com/github/viktornar/hiccup/HiccupPropertiesTest.java
@@ -3,12 +3,14 @@ package com.github.viktornar.hiccup;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(
         properties = "hiccup.gameService.baseUrlForAPIv2: https://foo.bar"
 )
+@ActiveProfiles("test")
 class HiccupPropertiesTest {
     @Autowired
     private HiccupProperties properties;
